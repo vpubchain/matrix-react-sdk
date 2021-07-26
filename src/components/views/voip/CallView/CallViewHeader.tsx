2a1b84288ef0bd09d71db9aba3bf90b7b55f4445
Dariusz Niemczyk <palid@phoelid.me>
2021-07-26 16:03:39 +0200

    Fix code review comments

--- src/components/views/voip/CallView/CallViewHeader.tsx
+++ src/components/views/voip/CallView/CallViewHeader.tsx
@@ -1,13 +1,28 @@
+/*
+Copyright 2021 New Vector Ltd
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 import { CallType } from 'matrix-js-sdk/src/webrtc/call';
 import { Room } from 'matrix-js-sdk/src/models/room';
 import React from 'react';
+import { isUndefined } from 'lodash';
 import { _t } from '../../../../languageHandler';
 import RoomAvatar from '../../avatars/RoomAvatar';
 import AccessibleButton from '../../elements/AccessibleButton';
 import dis from '../../../../dispatcher/dispatcher';
 import WidgetAvatar from '../../avatars/WidgetAvatar';
 import { IApp } from '../../../../stores/WidgetStore';
-import { isUndefined } from 'lodash';
 import WidgetUtils from '../../../../utils/WidgetUtils';
 
 const callTypeTranslationByType: Record<CallType, () => string> = {
